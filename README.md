9dde3b9ec4e457116532f4abfc0931d18749b8ab
Miguel Olarte <mbsic91@gmail.com>
2025-06-17 10:44:22 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Parking a lot
+Sistema para la gestión de parqueaderos
---
+++ src/test/java/com/katusoft/parking_a_lot/ParkingALotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katusoft.parking_a_lot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingALotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
